da891507a7332f58b0bd1e118c4921b7b495dcb9
shotabakh <bkhbakhashvilishota@gmail.com>
2025-06-24 13:27:46 +0400

    078

--- scripts.js
+++ scripts.js
@@ -1,23 +1,6 @@
-// ფუნქცია, რომელიც სთხოვს მომხმარებელს შედგეს რომელ გვერდზე უნდა გადადოს
-document.querySelectorAll("nav a").forEach((link) => {
-    link.addEventListener("click", function (e) {
-      e.preventDefault(); // აიკრძალება ლინკზე მიანიშნება, რათა გვერდი არ გადაიტვირთოს
-  
-      let პასუხი = prompt("საიტის რომელ გვერდზე გინდა გადასვლა? (მთავარი, შესახებ, კონტაქტი)");
-  
-      if (პასუხი !== null) {
-        პასუხი = პასუხი.toLowerCase();
-  
-        if (პასუხი === "მთავარი") {
-          window.location.href = "index.html";
-        } else if (პასუხი === "შესახებ") {
-          window.location.href = "2.html";
-        } else if (პასუხი === "კონტაქტი") {
-          window.location.href = "მესამე.html";
-        } else {
-          alert("ასეთი გვერდი არ არსებობს");
-        }
-      }
-    });
-  });
+const navbar = document.getElementById('navbar')
+
+function togglenavbar(){
+  navbar.classList.toggle('show')
+}
   
